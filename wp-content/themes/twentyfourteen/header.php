3aa069bda7e039532eb7f613cd1d6c7d9462a53c
Matheus da Mata <matheusdamata@outlook.com>
2014-09-12 17:09:12 -0300

    alterações de sexta

--- wp-content/themes/twentyfourteen/header.php
+++ wp-content/themes/twentyfourteen/header.php
@@ -36,57 +36,54 @@
 <body <?php body_class(); ?>>
 
 	<header id="masthead" class="site-header" role="banner">
-		<nav class="navbar navbar-default" role="navigation">
-		  <div class="container-fluid">
-		    <!-- Brand and toggle get grouped for better mobile display -->
-		    <div class="navbar-header">
-		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-		        <span class="sr-only">Toggle navigation</span>
-		        <span class="icon-bar"></span>
-		        <span class="icon-bar"></span>
-		        <span class="icon-bar"></span>
-		      </button>
-		      <a class="navbar-brand" href="#">Brand</a>
-		    </div>
+		<div class="faixa top-fix">
+			<div class="container">
 
-		    <!-- Collect the nav links, forms, and other content for toggling -->
-		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-		      <ul class="nav navbar-nav">
-		        <li class="active"><a href="#">Link</a></li>
-		        <li><a href="#">Link</a></li>
-		        <li class="dropdown">
-		          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-		          <ul class="dropdown-menu" role="menu">
-		            <li><a href="#">Action</a></li>
-		            <li><a href="#">Another action</a></li>
-		            <li><a href="#">Something else here</a></li>
-		            <li class="divider"></li>
-		            <li><a href="#">Separated link</a></li>
-		            <li class="divider"></li>
-		            <li><a href="#">One more separated link</a></li>
-		          </ul>
-		        </li>
-		      </ul>
-		      <form class="navbar-form navbar-left" role="search">
-		        <div class="form-group">
-		          <input type="text" class="form-control" placeholder="Search">
-		        </div>
-		        <button type="submit" class="btn btn-default">Submit</button>
-		      </form>
-		      <ul class="nav navbar-nav navbar-right">
-		        <li><a href="#">Link</a></li>
-		        <li class="dropdown">
-		          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-		          <ul class="dropdown-menu" role="menu">
-		            <li><a href="#">Action</a></li>
-		            <li><a href="#">Another action</a></li>
-		            <li><a href="#">Something else here</a></li>
-		            <li class="divider"></li>
-		            <li><a href="#">Separated link</a></li>
-		          </ul>
-		        </li>
-		      </ul>
-		    </div><!-- /.navbar-collapse -->
-		  </div><!-- /.container-fluid -->
-		</nav>
+			</div>
+		</div>
+		<div class="container">
+			<img src="<?php bloginfo('template_url'); ?>/images/logo-holhos.png" alt="Logo H'Olhos">
+			<nav id="menu-header" class="navbar" role="navigation">
+			  <div class="container-fluid">
+			    <!-- Brand and toggle get grouped for better mobile display -->
+			    <div class="navbar-header">
+			      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-menu-header">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			      </button>
+			    </div>
+
+			    <!-- Collect the nav links, forms, and other content for toggling -->
+			    <div class="collapse navbar-collapse" id="nav-menu-header">
+			      <ul class="nav navbar-nav">
+			       
+			        <li><a href="#">Quem Somos</a></li>
+			        <li><a href="#">Projetos</a></li>
+				        <li class="dropdown">
+				          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Neurovisão <span class="caret"></span></a>
+				          <ul class="dropdown-menu" role="menu">
+				            <li><a href="#">O que é</a></li>
+				            <li class="divider"></li>
+				            <li><a href="#">Processamento Visual</a></li>
+				            <li class="divider"></li>
+				            <li><a href="#">SÍNDROME DE IRLEN</a></li>
+				            <li class="divider"></li>
+				            <li><a href="#">TRATAMENTOS POSSÍVEIS</a></li>
+				            <li class="divider"></li>
+				            <li><a href="#">EFEITOS DE DISTORÇÃO</a></li>
+				          </ul>
+				        </li>
+				        <li><a href="#">Loja Virtual</a></li>
+				        <li><a href="#">Guia de Profissionais</a></li>
+				        <li><a href="#">Cursos</a></li>
+				        <li><a href="#">Artigos</a></li>
+				        <li><a href="#">Mídias</a></li>
+			      </ul>
+			     
+			    </div><!-- /.navbar-collapse -->
+			  </div><!-- /.container-fluid -->
+			</nav>
+		</div>
 	</header><!-- #masthead -->
